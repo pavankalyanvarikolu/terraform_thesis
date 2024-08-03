d48befb39277e91e9e606bdb2be63cb3736fd9f1
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-08-03 21:53:22 +0530

    adding main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Sample Terraform Code
+
 provider "aws" {
   region = "us-west-2"
 }
@@ -10,24 +12,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "vulnerable-subnet-1"
-  }
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "vulnerable-subnet-2"
-  }
-}
-
 resource "aws_security_group" "allow_all" {
   vpc_id = aws_vpc.main.id
 
@@ -35,14 +19,14 @@ resource "aws_security_group" "allow_all" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Vulnerability: Allowing all traffic from anywhere
+    cidr_blocks = ["0.0.0.0/0"]  # Overly permissive rule
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Vulnerability: Allowing all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]  # Overly permissive rule
   }
 
   tags = {
@@ -51,39 +35,41 @@ resource "aws_security_group" "allow_all" {
 }
 
 resource "aws_instance" "instance1" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example Amazon Linux 2 AMI
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet1.id
-  security_groups = [aws_security_group.allow_all.name]  # Vulnerability: Using security group that allows all traffic
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install httpd -y
-              service httpd start
-              chkconfig httpd on
-              EOF
+  subnet_id     = aws_vpc.main.id
 
   tags = {
     Name = "vulnerable-instance-1"
   }
 }
 
-resource "aws_instance" "instance2" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example Amazon Linux 2 AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet2.id
-  security_groups = [aws_security_group.allow_all.name]  # Vulnerability: Using security group that allows all traffic
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install httpd -y
-              service httpd start
-              chkconfig httpd on
-              EOF
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket"
+  acl    = "public-read"  # Publicly accessible S3 bucket
 
   tags = {
-    Name = "vulnerable-instance-2"
+    Name = "vulnerable-bucket"
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name = "example-policy"
+  
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",  # Wildcard permissions
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name = "vulnerable-policy"
   }
 }
